09bef4c0bb8f5e26dc3b4b19e42215d7fa9f8628
llxSKyWALKeRxll <19bcs2190@gmail.com>
2021-06-30 07:53:17 +0530

    Initial commit

---
+++ app/src/main/java/com/internshala/skyhub/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.internshala.skyhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.BoringLayout
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordLayout = findViewById(R.id.coordLayout)
+        toolbar = findViewById(R.id.toolbar)
+        frameLayout = findViewById(R.id.frameLayout)
+        navigationView = findViewById(R.id.navigationView)
+
+        setupToolbar()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this@MainActivity,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.itmDashboard -> {
+                    Toast.makeText(this@MainActivity,
+                        "Dashboard is not available as of yet. Coming soon!",
+                    Toast.LENGTH_SHORT).show()
+                }
+                R.id.itmFavourites -> {
+                    Toast.makeText(this@MainActivity,
+                        "Favourites is not available as of yet. Coming soon!",
+                        Toast.LENGTH_SHORT).show()
+                }
+                R.id.itmProfile -> {
+                    Toast.makeText(this@MainActivity,
+                        "Profile is not available as of yet. Coming soon!",
+                        Toast.LENGTH_SHORT).show()
+                }
+                R.id.itmAboutApp -> {
+                    Toast.makeText(this@MainActivity,
+                        "About App is not available as of yet. Coming soon!",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
+    }
+
+    fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "SkyHub"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if(id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
